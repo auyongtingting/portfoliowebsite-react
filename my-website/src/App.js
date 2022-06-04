468e9077737675b1cfaf53ff12cc246062e738af
Au Yong Ting Ting <tingting.a@affinidi.com>
2022-06-04 13:26:59 +0800

    Revert "Update naming convention for components"
    This reverts commit 339185f34f94666b3d9f96b3c9ee80a54109b865.

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,6 +1,6 @@
 // components
-import Navbar from "./components/Nav.js"
-import Home from "./components/Home.js"
+import Navbar from "./components/Nav"
+import Home from "./components/Home"
 import "./styles/navbar.css"
 
 function App() {
--- my-website/src/components/Home.js
+++ my-website/src/components/Home.js
@@ -1,5 +1,5 @@
 import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
-import DownloadButton from '../components/resumeBtn.js'
+import DownloadButton from '../components/resumeBtn'
 import profilePicture from '../images/profilepic.jpeg'
 import pdf from '../resume/AuYongTingTing_Resume.pdf'
 // Icons
