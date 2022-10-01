75895fd1508e8beb24b7ac2ac91a2dab1246ac5c
Au Yong Ting Ting <tingting.a@affinidi.com>
2022-10-01 16:05:26 +0800

    Update resume

--- my-website/src/components/Home.js
+++ my-website/src/components/Home.js
@@ -1,49 +1,81 @@
-import { React, useState } from "react"
-import { MDBCol, MDBRow, MDBContainer } from 'mdb-react-ui-kit'
+import { React, useState } from "react";
+import { MDBCol, MDBRow, MDBContainer } from "mdb-react-ui-kit";
 
 // styling
-import "../styles/styles.css"
-import "../styles/home.css"
+import "../styles/styles.css";
+import "../styles/home.css";
 
-// materials 
-import DownloadButton from '../components/resumeBtn.js'
-import profilePicture from '../images/profilepic.jpeg'
-import pdf from '../resume/AuYongTingTing_Resume2.pdf'
+// materials
+import DownloadButton from "../components/resumeBtn.js";
+import profilePicture from "../images/profilepic.jpeg";
+import pdf from "../resume/AuYongTingTing_Resume.pdf";
 
 //icons
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import {AiFillSmile} from 'react-icons/ai'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { AiFillSmile } from "react-icons/ai";
 
 export default function Home() {
-
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url)
-        if (newWindow) newWindow.opener = null
-    }
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url);
+    if (newWindow) newWindow.opener = null;
+  };
 
   return (
-    <div id = "home">
-        <MDBContainer>
-            <MDBRow>
-                <MDBCol size='md' className='col-example'>
-                    <img src={profilePicture}/>
-                </MDBCol>
-                <MDBCol size='md' className='col-example'>
-                    <div class="text">
-                        <h1>Hi!
-                        <span className="logoSmile"><AiFillSmile color="#5476B9" fontSize="1em" /></span>
-                        <br></br>I'm Au Yong Ting Ting!</h1>
-                        <h2>A Software Engineer</h2>
-                        <DownloadButton className="button"><a className="downloadResume" href={pdf} download="Resume.pdf">Download Resume</a></DownloadButton>
-                        <ul>
-                            <li className="IntroductionIcons"><FaLinkedin color="#5476B9" fontSize="1.5em"  onClick={()=>openInNewTab("https://www.linkedin.com/in/auyongtingting/")}/></li>
-                            <li className="IntroductionIcons"><FaGithub color="#5476B9" fontSize="1.5em" onClick={()=>openInNewTab("https://github.com/auyongtingting")}/></li>
-                        </ul>
-                    </div>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <div id="home">
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol size="md" className="col-example">
+            <img src={profilePicture} />
+          </MDBCol>
+          <MDBCol size="md" className="col-example">
+            <div class="text">
+              <h1>
+                Hi!
+                <span className="logoSmile">
+                  <AiFillSmile color="#5476B9" fontSize="1em" />
+                </span>
+                <br></br>I'm Au Yong Ting Ting!
+              </h1>
+              <h2>A Software Engineer</h2>
+              <DownloadButton className="button">
+                <a className="downloadResume" href={pdf} download="Resume.pdf">
+                  Download Resume
+                </a>
+              </DownloadButton>
+              <ul>
+                <li className="IntroductionIcons">
+                  <FaLinkedin
+                    color="#5476B9"
+                    fontSize="1.5em"
+                    onClick={() =>
+                      openInNewTab(
+                        "https://www.linkedin.com/in/auyongtingting/"
+                      )
+                    }
+                  />
+                </li>
+                <li className="IntroductionIcons">
+                  <FaGithub
+                    color="#5476B9"
+                    fontSize="1.5em"
+                    onClick={() =>
+                      openInNewTab("https://github.com/auyongtingting")
+                    }
+                  />
+                </li>
+              </ul>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
   );
 }
